4784e94d3d04da3c32ab1cf5afef56b3af0def62
Arathi <avallipuranathan@gmail.com>
2024-09-25 22:30:46 -0400

    Update a3.sql

--- a3.sql
+++ a3.sql
@@ -5,6 +5,25 @@ CREATE TABLE CompetitionAdmin {
 	Email VARCHAR2(30),
 	PhoneNumber VARCHAR2(15),
 	Username VARCHAR2(24) NOT NULL,
-	Password VARCHAR(24) NOT NULL,
+	Password VARCHAR2(24) NOT NULL,
 	PRIMARY KEY(CompetitionAdminID)
+};
+
+
+CREATE TABLE League {
+	LeagueID NUMBER,
+	CompetitionAdminID NUMBER REFERENCES CompetitionAdmin(CompetitionAdminID),
+	PRIMARY KEY(LeagueID)
+};
+
+CREATE TABLE Player {
+	PlayerID NUMBER,
+	FirstName VARCHAR2(25),
+	LastName VARCHAR2(25),
+	Email VARCHAR2(30),
+	PhoneNumber VARCHAR2(15),
+	Username VARCHAR2(24) NOT NULL,
+	Password VARCHAR2(24) NOT NULL,
+	Position VARCHAR2(15),
+	PRIMARY KEY(PlayerID)
 };
