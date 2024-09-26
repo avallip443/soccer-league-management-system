16e4f20f1c96840e180c0e39332b8660efdbd85b
Arathi <avallipuranathan@gmail.com>
2024-09-25 22:40:40 -0400

    Update a3.sql

--- a3.sql
+++ a3.sql
@@ -1,4 +1,4 @@
-CREATE TABLE CompetitionAdmin {
+CREATE TABLE CompetitionAdmin (
 	CompetitionAdminID NUMBER,
 	FirstName VARCHAR2(25),
 	LastName VARCHAR2(25),
@@ -7,18 +7,35 @@ CREATE TABLE CompetitionAdmin {
 	Username VARCHAR2(24) NOT NULL,
 	Password VARCHAR2(24) NOT NULL,
 	PRIMARY KEY(CompetitionAdminID)
-};
+);
 
 
-CREATE TABLE League {
+CREATE TABLE League (
 	LeagueID NUMBER,
 	CompetitionAdminID NUMBER REFERENCES CompetitionAdmin(CompetitionAdminID),
 	LeagueName VARCHAR2(30),
 	PRIMARY KEY(LeagueID, CompetitionAdminID)
-};
+);
 
 
-CREATE TABLE Player {
+CREATE TABLE Team (
+	TeamID NUMBER,
+	LeagueID NUMBER REFERENCES League(LeagueID),
+	CompetitionAdminID NUMBER REFERENCES CompetitionAdmin(CompetitionAdminID),
+	TeamName VARCHAR2(30),
+	Points NUMBER,
+	Wins NUMBER,
+	Losses NUMBER,
+	Draws NUMBER,
+	GoalsFor NUMBER,
+	GoalsAgainst NUMBER,
+	Venue VARCHAR2(20),
+	GoalDifference NUMBER AS (GoalsFor - GoalsAgainst),
+	PRIMARY KEY(TeamID, LeagueID, CompetitionAdminID)
+);
+
+
+CREATE TABLE Player (
 	PlayerID NUMBER,
 	FirstName VARCHAR2(25),
 	LastName VARCHAR2(25),
@@ -28,4 +45,4 @@ CREATE TABLE Player {
 	Password VARCHAR2(24) NOT NULL,
 	Position VARCHAR2(15),
 	PRIMARY KEY(PlayerID)
-};
+);
