4ecb7f6afcfe6ee0a56d8889a9abc3be397a40a5
Arathi <avallipuranathan@gmail.com>
2024-10-01 22:23:20 -0400

    add premier players

--- sql/player.sql
+++ sql/player.sql
@@ -1,3 +1,4 @@
+-- Insert players for teams for Premier League
 -- Insert players for Arsenal
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
 VALUES (1, 1, 1, 1, 'David', 'Raya', 'draya@arsenal.com', '416-400-0000', 'draya', 'password123', 'Goalkeeper');
@@ -10,6 +11,34 @@ VALUES (3, 1, 1, 1, 'Thomas', 'Partey', 'tpartey@arsenal.com', '1234567892', 'tp
  
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
 VALUES (4, 1, 1, 1, 'William', 'Saliba', 'swaliba@arsenal.com', '1234567892', 'swaliba', 'password789', 'Defender');
+
+-- Insert players for Liverpool
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (1, 2, 1, 1, 'Alisson', 'Becker', 'abecker@arsenal.com', '416-400-0000', 'abecker', 'password123', 'Goalkeeper');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (3, 2, 1, 1, 'Luis', 'Díaz', 'ldiaz@arsenal.com', '1234567892', 'ldiaz', 'password789', 'Forward');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (3, 2, 1, 1, 'Ryan', 'Gravenberch', 'rgravenberch@arsenal.com', '1234567892', 'rgravenberch', 'password789', 'Midfielder');
+ 
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (4, 2, 1, 1, 'Virgil', 'van Dijk', 'vdijk@arsenal.com', '1234567892', 'vdijk', 'password789', 'Defender');
+
+-- Insert players for Manchester City
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (1, 3, 1, 1, 'Ederson', 'Santana de Moraes', 'emoraes@arsenal.com', '416-400-0000', 'emoraes', 'password123', 'Goalkeeper');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (2, 3, 1, 1, 'Erling', 'Haaland', 'ehaaland@arsenal.com', '1234567891', 'ltrossard', 'password456', 'Forward');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (3, 3, 1, 1, 'Bernardo', 'Silva', 'bsilva@arsenal.com', '1234567892', 'bsilva', 'password789', 'Midfielder');
+ 
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (4, 3, 1, 1, 'Josko', 'Gvardiol', 'jgvardiol@arsenal.com', '1234567892', 'jgvardiol', 'password789', 'Defender');
+
+
 
 -- Insert players for Barcelona
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
--- sql/team.sql
+++ sql/team.sql
@@ -8,6 +8,7 @@ VALUES (2, 1, 1, 'Liverpool', 0, 0, 0, 0, 0, 0, 'Anfield');
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
 VALUES (3, 1, 1, 'Manchester City', 0, 0, 0, 0, 0, 0, 'Etihad Stadium');
 
+
 -- Insert LaLiga Madrid
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
 VALUES (4, 2, 2, 'Real Madrid', 0, 0, 0, 0, 0, 0, 'Santiago Bernabeu');
@@ -18,12 +19,13 @@ VALUES (5, 2, 2, 'Barcelona', 0, 0, 0, 0, 0, 0, 'Camp Nou');
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
 VALUES (6, 2, 2, 'Atletico Madrid', 0, 0, 0, 0, 0, 0, 'Wanda Metropolitano');
 
+
 -- Insert Series A
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (7, 3, 2, 'Juventus', 0, 0, 0, 0, 0, 0, 'Allianz Stadium');
+VALUES (7, 3, 3, 'Juventus', 0, 0, 0, 0, 0, 0, 'Allianz Stadium');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (8, 3, 2, 'AC Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
+VALUES (8, 3, 3, 'AC Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (9, 3, 2, 'Inter Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
+VALUES (9, 3, 3, 'Inter Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
