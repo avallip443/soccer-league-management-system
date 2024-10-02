1afcaad3acb0073bf3648f46c83eaa14767a167f
Arathi <avallipuranathan@gmail.com>
2024-10-01 23:25:28 -0400

    add referees

---
+++ sql/referee.sql
@@ -0,0 +1,15 @@
+-- Insert fake referees
+INSERT INTO Referee (RefereeID, FirstName, LastName, Email, PhoneNumber, Username, RefereePassword) 
+VALUES (1, 'Jane', 'Doe', 'janedoe@referees.com', '555-001-0001', 'janedoe', 'password123');
+
+INSERT INTO Referee (RefereeID, FirstName, LastName, Email, PhoneNumber, Username, RefereePassword) 
+VALUES (2, 'Michael', 'Johnson', 'mjohnson@referees.com', '555-001-0002', 'mjohnson', 'password456');
+
+INSERT INTO Referee (RefereeID, FirstName, LastName, Email, PhoneNumber, Username, RefereePassword) 
+VALUES (3, 'David', 'Williams', 'dwilliams@referees.com', '555-001-0003', 'dwilliams', 'password789');
+
+INSERT INTO Referee (RefereeID, FirstName, LastName, Email, PhoneNumber, Username, RefereePassword) 
+VALUES (4, 'James', 'Brown', 'jbrown@referees.com', '555-001-0004', 'jbrown', 'password101');
+
+INSERT INTO Referee (RefereeID, FirstName, LastName, Email, PhoneNumber, Username, RefereePassword) 
+VALUES (5, 'Rebecca', 'Jones', 'rjones@referees.com', '555-001-0005', 'rjones', 'password202');
