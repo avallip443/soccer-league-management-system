151e3c69542d7cef98a9ac87ea41c729fa107637
Arathi <avallipuranathan@gmail.com>
2024-10-01 22:36:27 -0400

    update phone numbers

--- sql/competition_admin.sql
+++ sql/competition_admin.sql
@@ -1,8 +1,8 @@
 INSERT INTO CompetitionAdmin (CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, AdminPassword) 
-VALUES (1, 'John', 'Doe', 'johndoe@example.com', '416-100-0000', 'johndoe', 'password123');
+VALUES (1, 'John', 'Doe', 'johndoe@example.com', '100-000-0000', 'johndoe', 'password123');
 
 INSERT INTO CompetitionAdmin (CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, AdminPassword) 
-VALUES (2, 'Jane', 'Smith', 'janesmith@example.com', '416-200-0000', 'janesmith', 'securepassword456');
+VALUES (2, 'Jane', 'Smith', 'janesmith@example.com', '200-000-0000', 'janesmith', 'securepassword456');
 
 INSERT INTO CompetitionAdmin (CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, AdminPassword) 
-VALUES (2, 'Bob', 'Smith', 'bobsmith@example.com', '416-300-0000', 'bobsmith', 'password456');
+VALUES (2, 'Bob', 'Smith', 'bobsmith@example.com', '300-000-0000', 'bobsmith', 'password456');
