692c8a733e6aba7b03e788e64d3bb012239acfb4
Arathi <avallipuranathan@gmail.com>
2024-10-02 13:51:07 -0400

    update sql

--- sql/team.sql
+++ sql/team.sql
@@ -1,32 +1,46 @@
 -- Insert Premier League teams 
-INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (1, 1, 1, 'Arsenal', 0, 0, 0, 0, 0, 0, 'Emirates Stadium');
+INSERT INTO Team (TeamID, LeagueID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (1, 1, 'Arsenal', 0, 0, 0, 0, 0, 0, 'Emirates Stadium');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (2, 1, 1, 'Liverpool', 0, 0, 0, 0, 0, 0, 'Anfield');
+VALUES (2, 1, 'Liverpool', 0, 0, 0, 0, 0, 0, 'Anfield');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (3, 1, 1, 'Manchester City', 0, 0, 0, 0, 0, 0, 'Etihad Stadium');
+VALUES (3, 1, 'Manchester City', 0, 0, 0, 0, 0, 0, 'Etihad Stadium');
 
 
--- Insert LaLiga Madrid
-INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (4, 2, 2, 'Real Madrid', 0, 0, 0, 0, 0, 0, 'Santiago Bernabeu');
-
-INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (5, 2, 2, 'Barcelona', 0, 0, 0, 0, 0, 0, 'Camp Nou');
+-- Insert LaLiga teams
+INSERT INTO Team (TeamID, LeagueID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (4, 2, 'Real Madrid', 0, 0, 0, 0, 0, 0, 'Santiago Bernabeu');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (6, 2, 2, 'Atletico Madrid', 0, 0, 0, 0, 0, 0, 'Wanda Metropolitano');
-
-
--- Insert Serie A
-INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (7, 3, 3, 'Juventus', 0, 0, 0, 0, 0, 0, 'Allianz Stadium');
+VALUES (5, 2, 'Barcelona', 0, 0, 0, 0, 0, 0, 'Camp Nou');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (8, 3, 3, 'AC Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
+VALUES (6, 2, 'Atletico Madrid', 0, 0, 0, 0, 0, 0, 'Wanda Metropolitano');
+
+
+-- Insert Serie A teams
+INSERT INTO Team (TeamID, LeagueID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (7, 3, 'Juventus', 0, 0, 0, 0, 0, 0, 'Allianz Stadium');
 
 INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
-VALUES (9, 3, 3, 'Inter Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
+VALUES (8, 3, 'AC Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
+
+INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (9, 3, 'Inter Milan', 0, 0, 0, 0, 0, 0, 'San Siro');
+
+
+-- Insert Mississauga league teams
+INSERT INTO Team (TeamID, LeagueID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (10, 4, 'Mississauga Warriors', 10, 3, 2, 1, 14, 7, 'Churchill Medows Arena');
+
+INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (11, 4, 'Mississauga Dragons', 8, 2, 2, 2, 14, 12, 'Erin Mills Arena');
+
+INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (12, 4, 'Mississauga Eagles', 7, 2, 3, 1, 11, 20, 'Square One Arena');
+
+INSERT INTO Team (TeamID, LeagueID, CompetitionAdminID, TeamName, Points, Wins, Losses, Draws, GoalsFor, GoalsAgainst, Venue) 
+VALUES (13, 4, 'Mississauga Bull Dogs', 8, 2, 2, 2, 14, 14, 'Liverpool Frauds Arena');
 
--- sql/team_management.sql
+++ sql/team_management.sql
@@ -19,6 +19,8 @@ VALUES (5, 3, 'Pep', 'Guardiola', 'pguardiola@manchestercity.com', '000-005-0000
 INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
 VALUES (6, 3, 'Carlos', 'Vicens', 'cvicens@manchestercity.com', '000-006-0000', 'cvicens', 'password890', 'Assistant Manager');
 
+
+
 -- Insert management for Real Madrid
 INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
 VALUES (7, 5, 'Carlo', 'Ancelotti', 'cancelotti@realmadrid.com', '000-007-0000', 'cancelotti', 'password123', 'Manager');
@@ -32,6 +34,8 @@ VALUES (9, 6, 'Diego', 'Simeone', 'dsimeone@atleticomadrid.com', '000-009-0000',
 
 INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
 VALUES (10, 6, 'Nelson', 'Vivas', 'nvivas@atleticomadrid.com', '000-010-0000', 'nvivas', 'password567', 'Assistant Manager');
+
+
 
 -- Insert management for Juventus
 INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
@@ -53,3 +57,33 @@ VALUES (15, 9, 'Simone', 'Inzaghi', 'sinzaghi@intermilan.com', '000-015-0000', '
 
 INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
 VALUES (16, 9, 'Massimiliano', 'Farris', 'mfarris@intermilan.com', '000-016-0000', 'mfarris', 'password567', 'Assistant Manager');
+
+
+-- Insert management for Mississauga Warriors
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (17, 10, 'Sindi1', 'Gur', 'sindi1@mwarriors.com', '000-017-0000', 'sindig1', 'password456', 'Manager');
+
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (18, 10, 'Sindi2', 'Gur', 'sindi2@mwarriors.com', '000-018-0000', 'sindig2', 'password456', 'Assistant Manager');
+
+-- Insert management for Mississauga Dragons
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (19, 11, 'Arathi1', 'Vall', 'arathi1@mdragons.com', '000-019-0000', 'arathiv1', 'password567', 'Manager');
+
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (20, 11, 'Arathi2', 'Vall', 'arathi2@mdragons.com', '000-020-0000', 'arathiv2', 'password567', 'Assistant Manager');
+
+-- Insert management for Mississauga Dragons
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (21, 12, 'Sindi3', 'Gur', 'sindi3@mwarriors.com', '000-021-0000', 'sindig3', 'password456', 'Manager');
+
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (22, 12, 'Sindi4', 'Gur', 'sindi4@mwarriors.com', '000-022-0000', 'sindig4', 'password456', 'Assistant Manager');
+
+-- Insert management for Mississauga Dragons
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (23, 13, 'Arathi3', 'Vall', 'arathi3@mdragons.com', '000-023-0000', 'arathiv3', 'password567', 'Manager');
+
+INSERT INTO TeamManagement (TeamManagementID, TeamID, FirstName, LastName, Email, PhoneNumber, Username, ManagementPassword, TeamRole) 
+VALUES (24, 13, 'Arathi4', 'Vall', 'arathi4@mdragons.com', '000-024-0000', 'arathiv4', 'password567', 'Assistant Manager');
+
