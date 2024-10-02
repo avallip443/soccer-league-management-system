bfb00d983308e4a5080911b55f802c8b257e4b48
Arathi <avallipuranathan@gmail.com>
2024-10-02 14:27:58 -0400

    update teams for game stats

--- sql/game_team_stats.sql
+++ sql/game_team_stats.sql
@@ -1,54 +1,100 @@
 -- Games for Team 1 at home
 --Game 1
-INSERT INTO GameTeamStats VALUES (1,1,1);
-INSERT INTO GameTeamStats VALUES (1,2,0);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (1, 10, 1);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (1, 11, 0);
+
 
 --Game 2
-INSERT INTO GameTeamStats VALUES (2,1,0);
-INSERT INTO GameTeamStats VALUES (2,3,0);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (2, 10, 0);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (2, 12, 0);
+
 
 --Game 3
-INSERT INTO GameTeamStats VALUES (3,1,4);
-INSERT INTO GameTeamStats VALUES (3,4,3);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (3, 10, 4);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (3, 13, 3);
+
+
 
 --Games for Team 2 at home
 --Game 4
-INSERT INTO GameTeamStats VALUES (4,2,3);
-INSERT INTO GameTeamStats VALUES (4,1,0);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (4, 11, 3);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (4, 10, 0);
+
 
 --Game 5
-INSERT INTO GameTeamStats VALUES (5,2,4);
-INSERT INTO GameTeamStats VALUES (5,3,5);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (5, 11, 4);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (5, 12, 5);
+
 
 --Game 6
-INSERT INTO GameTeamStats VALUES (6,2,3);
-INSERT INTO GameTeamStats VALUES (6,4,3);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (6, 11, 3);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (6, 13, 3);
+
 
 --Games for Team 3 at home
 --Game 7
-INSERT INTO GameTeamStats VALUES (7,3,0);
-INSERT INTO GameTeamStats VALUES (7,1,9);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (7, 12, 0);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (7, 10, 9);
+
 
 --Game 8
-INSERT INTO GameTeamStats VALUES (8,3,1);
-INSERT INTO GameTeamStats VALUES (8,2,2);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (8, 12, 1);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (8, 11, 2);
+
 
 --Game 9
-INSERT INTO GameTeamStats VALUES (9,3,2);
-INSERT INTO GameTeamStats VALUES (9,4,3);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (9, 12, 2);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (9, 13, 3);
+
 
 --Games for Team 4 at home
 --Game 10
-INSERT INTO GameTeamStats VALUES (10,4,1);
-INSERT INTO GameTeamStats VALUES (10,1,0);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (10, 13, 1);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (10, 10, 0);
+
 
 --Game 11
-INSERT INTO GameTeamStats VALUES (11,4,2);
-INSERT INTO GameTeamStats VALUES (11,2,2);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (11, 13, 2);
+
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (11, 11, 2);
+
 
 --Game 12
-INSERT INTO GameTeamStats VALUES (12,4,2);
-INSERT INTO GameTeamStats VALUES (12,3,3);
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (12, 13, 2);
 
-select * from GameTeamStats;
-delete from GameTeamStats;
+INSERT INTO GameTeamStats (GameID, TeamID, Goals)
+VALUES (12, 12, 3);
+
