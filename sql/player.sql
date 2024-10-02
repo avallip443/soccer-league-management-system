24a607f62791c28018ae77ed893bb3f95c4be417
Arathi <avallipuranathan@gmail.com>
2024-10-01 22:36:39 -0400

    update players

--- sql/player.sql
+++ sql/player.sql
@@ -1,54 +1,81 @@
 -- Insert players for teams for Premier League
 -- Insert players for Arsenal
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (1, 1, 1, 1, 'David', 'Raya', 'draya@arsenal.com', '416-400-0000', 'draya', 'password123', 'Goalkeeper');
+VALUES (1, 1, 1, 1, 'David', 'Raya', 'draya@arsenal.com', '000-000-0001', 'draya', 'password123', 'Goalkeeper');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (2, 1, 1, 1, 'Leandro', 'Trossard', 'ltrossard@arsenal.com', '1234567891', 'ltrossard', 'password456', 'Forward');
+VALUES (2, 1, 1, 1, 'Leandro', 'Trossard', 'ltrossard@arsenal.com', '000-000-0002', 'ltrossard', 'password456', 'Forward');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (3, 1, 1, 1, 'Thomas', 'Partey', 'tpartey@arsenal.com', '1234567892', 'tpartey', 'password789', 'Midfielder');
+VALUES (3, 1, 1, 1, 'Thomas', 'Partey', 'tpartey@arsenal.com', '000-000-0003', 'tpartey', 'password789', 'Midfielder');
  
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (4, 1, 1, 1, 'William', 'Saliba', 'swaliba@arsenal.com', '1234567892', 'swaliba', 'password789', 'Defender');
+VALUES (4, 1, 1, 1, 'William', 'Saliba', 'swaliba@arsenal.com', '000-000-0004', 'swaliba', 'password789', 'Defender');
 
 -- Insert players for Liverpool
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (1, 2, 1, 1, 'Alisson', 'Becker', 'abecker@arsenal.com', '416-400-0000', 'abecker', 'password123', 'Goalkeeper');
+VALUES (5, 2, 1, 1, 'Alisson', 'Becker', 'abecker@arsenal.com', '000-000-0005', 'abecker', 'password123', 'Goalkeeper');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (3, 2, 1, 1, 'Luis', 'Díaz', 'ldiaz@arsenal.com', '1234567892', 'ldiaz', 'password789', 'Forward');
+VALUES (6, 2, 1, 1, 'Luis', 'Díaz', 'ldiaz@arsenal.com', '000-000-0006', 'ldiaz', 'password789', 'Forward');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (3, 2, 1, 1, 'Ryan', 'Gravenberch', 'rgravenberch@arsenal.com', '1234567892', 'rgravenberch', 'password789', 'Midfielder');
+VALUES (7, 2, 1, 1, 'Ryan', 'Gravenberch', 'rgravenberch@arsenal.com', '000-000-0007', 'rgravenberch', 'password789', 'Midfielder');
  
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (4, 2, 1, 1, 'Virgil', 'van Dijk', 'vdijk@arsenal.com', '1234567892', 'vdijk', 'password789', 'Defender');
+VALUES (8, 2, 1, 1, 'Virgil', 'van Dijk', 'vdijk@arsenal.com', '000-000-0008', 'vdijk', 'password789', 'Defender');
 
 -- Insert players for Manchester City
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (1, 3, 1, 1, 'Ederson', 'Santana de Moraes', 'emoraes@arsenal.com', '416-400-0000', 'emoraes', 'password123', 'Goalkeeper');
+VALUES (9, 3, 1, 1, 'Ederson', 'Santana de Moraes', 'emoraes@arsenal.com', '000-000-0009', 'emoraes', 'password123', 'Goalkeeper');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (2, 3, 1, 1, 'Erling', 'Haaland', 'ehaaland@arsenal.com', '1234567891', 'ltrossard', 'password456', 'Forward');
+VALUES (10, 3, 1, 1, 'Erling', 'Haaland', 'ehaaland@arsenal.com', '000-000-0010', 'ltrossard', 'password456', 'Forward');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (3, 3, 1, 1, 'Bernardo', 'Silva', 'bsilva@arsenal.com', '1234567892', 'bsilva', 'password789', 'Midfielder');
+VALUES (11, 3, 1, 1, 'Bernardo', 'Silva', 'bsilva@arsenal.com', '000-000-0011', 'bsilva', 'password789', 'Midfielder');
  
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (4, 3, 1, 1, 'Josko', 'Gvardiol', 'jgvardiol@arsenal.com', '1234567892', 'jgvardiol', 'password789', 'Defender');
+VALUES (12, 3, 1, 1, 'Josko', 'Gvardiol', 'jgvardiol@arsenal.com', '000-000-0012', 'jgvardiol', 'password789', 'Defender');
 
 
+
+-- Insert players for teams for LaLiga
+-- Insert players for Real Madrid
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (13, 4, 2, 2, 'Thibaut', 'Courtois', 'tcourtois@realmadrid.com', '000-000-0013', 'tcourtois', 'password123', 'Goalkeeper');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (14, 4, 2, 2, 'Vinícius', 'Júnior', 'vjunior@realmadrid.com', '000-000-0014', 'vjunior', 'password456', 'Forward');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (15, 4, 2, 2, 'Luka', 'Modrić', 'lmodric@realmadrid.com', '000-000-0015', 'lmodric', 'password789', 'Midfielder');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (16, 4, 2, 2, 'Éder', 'Militão', 'emilitao@realmadrid.com', '000-000-0016', 'emilitao', 'password789', 'Defender');
 
 -- Insert players for Barcelona
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (1, 5, 2, 2, 'Marc-André', 'ter Stegen', 'mstegen@arsenal.com', '416-400-0000', 'mstegen', 'password123', 'Goalkeeper');
+VALUES (17, 5, 2, 2, 'Marc-André', 'ter Stegen', 'mstegen@arsenal.com', '000-000-0017', 'mstegen', 'password123', 'Goalkeeper');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (2, 5, 2, 2, 'Lamine', 'Yamal', 'lyamal@arsenal.com', '1234567891', 'lyamal', 'password456', 'Forward');
+VALUES (18, 5, 2, 2, 'Lamine', 'Yamal', 'lyamal@arsenal.com', '000-000-0018', 'lyamal', 'password456', 'Forward');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (3, 5, 2, 2, 'Marc', 'Casadó', 'mcasado@arsenal.com', '1234567892', 'mcasado', 'password789', 'Midfielder');
+VALUES (19, 5, 2, 2, 'Marc', 'Casadó', 'mcasado@arsenal.com', '000-000-0019', 'mcasado', 'password789', 'Midfielder');
 
 INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
-VALUES (3, 5, 2, 2, 'Pau', 'Cubarsí', 'pcubarsi@arsenal.com', '1234567892', 'pcubarsi', 'password789', 'Defender');
+VALUES (20, 5, 2, 2, 'Pau', 'Cubarsí', 'pcubarsi@arsenal.com', '000-000-0020', 'pcubarsi', 'password789', 'Defender');
+
+-- Insert players for Atletico Madrid
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (21, 6, 2, 2, 'Jan', 'Oblak', 'joblak@atleticomadrid.com', '000-000-0021', 'joblak', 'password123', 'Goalkeeper');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (22, 6, 2, 2, 'Julián', 'Álvarez', 'jalvarez@atleticomadrid.com', '000-000-0022', 'jalvarez', 'password456', 'Forward');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (23, 6, 2, 2, 'Marcos', 'Llorente', 'mllorente@atleticomadrid.com', '000-000-0023', 'mllorente', 'password789', 'Midfielder');
+
+INSERT INTO Player (PlayerID, TeamID, LeagueID, CompetitionAdminID, FirstName, LastName, Email, PhoneNumber, Username, PlayerPassword, PlayerPosition) 
+VALUES (24, 6, 2, 2, 'Reinildo', 'Mandava', 'rmandava@atleticomadrid.com', '000-000-0024', 'rmandava', 'password789', 'Defender');
