eee65121cacdf97623beab1c2a348f79d7b1b9ff
Arathi <avallipuranathan@gmail.com>
2024-10-17 23:24:44 -0400

    create sql table scripts

---
+++ bash/drop_tables.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+# export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
+
+sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.c)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
+
+DROP TABLE GamePlayerStats;
+DROP TABLE GameTeamStats;
+DROP TABLE Game;
+DROP TABLE Referee;
+DROP TABLE TeamManagement;
+DROP TABLE Player;
+DROP TABLE Team;
+DROP TABLE League;
+DROP TABLE CompetitionAdmin;
+
+exit;
+EOF
