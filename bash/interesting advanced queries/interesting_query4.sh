d6a8018638329d28a61223795fe8abf488050c38
Arathi <avallipuranathan@gmail.com>
2024-10-21 10:24:43 -0400

    Update interesting_query4.sh

--- bash/interesting advanced queries/interesting_query4.sh
+++ bash/interesting advanced queries/interesting_query4.sh
@@ -3,7 +3,7 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/passsword@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
--- 5) Get players who have scored but never received a yellow card using MINUS operator.
+-- 4) Get players who have scored but never received a yellow card using MINUS operator.
 
 SELECT p.PlayerID, p.FirstName, p.LastName
 FROM Player p
