fa8bc9c9f0340db7b5e0d3f627951e11413a0aed
Arathi <avallipuranathan@gmail.com>
2024-10-21 10:26:19 -0400

    Update interesting_query5.sh

--- bash/interesting advanced queries/interesting_query5.sh
+++ bash/interesting advanced queries/interesting_query5.sh
@@ -3,7 +3,7 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
--- 5) Get players who recieve no red cards using the NOT EXISTS operator.
+-- 5) Get players who receive no red cards using the NOT EXISTS operator.
 
 SELECT p.PlayerID, p.FirstName, p.LastName, t.TeamName
 FROM Player p
