9a24313153d4d873d946255ee4b10c3f6327a6fa
SindiGura <132945140+SindiGura@users.noreply.github.com>
2024-10-24 20:58:05 -0700

    Update interesting_query1.sh

--- bash/interesting advanced queries/interesting_query1.sh
+++ bash/interesting advanced queries/interesting_query1.sh
@@ -3,6 +3,15 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
+-- Retrieve all players who either scored more than 2 goals and/or assisted 1 goal or more in a game using UNION
+
+SELECT p.PlayerID,p.FirstName,p.LastName,gps.PlayerGoals,gps.PlayerAssists
+FROM GamePlayerStats gps,Player p
+WHERE gps.PlayerGoals > 2 and p.PlayerID = gps.PlayerID
+UNION
+SELECT p.PlayerID,p.FirstName,p.LastName,gps.PlayerGoals,gps.PlayerAssists
+FROM GamePlayerStats gps, Player p
+WHERE gps.PlayerAssists > 0 and p.PlayerID = gps.PlayerID;                                                                                       
 
 exit;
 EOF
