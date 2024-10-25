2d0f111eb542b0c17e26e14a24f72bbd8f86ae73
SindiGura <132945140+SindiGura@users.noreply.github.com>
2024-10-24 21:09:58 -0700

    Update interesting_query2.sh

--- bash/interesting advanced queries/interesting_query2.sh
+++ bash/interesting advanced queries/interesting_query2.sh
@@ -3,6 +3,15 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/12.1/client64/lib
 sqlplus64 "username/password@(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(Host=oracle.scs.ryerson.ca)(Port=1521))(CONNECT_DATA=(SID=orcl)))" <<EOF
 
+-- To see the players who scored higher than 0.5, used GROUP BY  
+
+CREATE VIEW GoodPlayerPerformance AS
+SELECT tm.Username,p.FirstName, p.LastName,p.PlayerPosition, t.TeamName, AVG(gps.PlayerGoals) AS GoalsPerGame
+FROM Player p, GamePlayerStats gps, Team t, TeamManagement tm
+WHERE p.PlayerID = gps.PlayerID AND p.TeamID = t.TeamID AND tm.TeamID = p.TeamID
+GROUP BY tm.Username,p.FirstName, p.LastName,p.PlayerPosition, t.TeamName
+HAVING AVG(gps.PlayerGoals) > 0.5
+ORDER BY GoalsPerGame DESC;
 
 exit;
 EOF
