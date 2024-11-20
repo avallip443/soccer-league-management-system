9b2dd4c5fc2e6bfa4b3b0aa1513ea80ecf9a46bb
Arathi <avallipuranathan@gmail.com>
2024-11-20 11:52:50 -0500

    create drop table script for 3nf

---
+++ ui/drop_table.sql
@@ -0,0 +1,32 @@
+-- GamePlayerStats table
+DROP TABLE GamePlayerStats;
+
+-- GameTeamStats table
+DROP TABLE GameTeamStats;
+
+-- Game tables
+DROP TABLE Game;
+DROP TABLE GameStatus;
+
+-- Referee tables
+DROP TABLE Referee;
+DROP TABLE RefereeContact;
+
+-- Team management tables
+DROP TABLE TeamManagement;
+DROP TABLE TeamManagementContact;
+
+-- Player tables
+DROP TABLE Player;
+DROP TABLE PlayerContact;
+
+-- Team tables
+DROP TABLE Team;
+DROP TABLE TeamStats;
+
+-- League table
+DROP TABLE League;
+
+-- Competition admin tables 
+DROP TABLE LeagueAdmin;
+DROP TABLE AdminContact;
